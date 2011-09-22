16fbbefa97a9c6ca1dcc354ebeb10584a6e9af34
David Alkire <david.alkire@gmail.com>
2011-09-22 08:56:00 -0400

    Initial Commit

---
+++ Ob Log/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ob Log
+//
+//  Created by David Alkire on 9/22/11.
+//  Copyright (c) 2011 Harvard Medical School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Ob Log/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ob Log
+//
+//  Created by David Alkire on 9/22/11.
+//  Copyright (c) 2011 Harvard Medical School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
